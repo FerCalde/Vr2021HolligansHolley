ee92bf2c1c52a0c872c6c6d151a26e3ab38dc40a
Calde <chipneidev@gmail.com>
2021-05-01 22:54:47 +0200

    InicioSpawner

---
+++ HolligansHolley/Assets/HolligansGameAssets/Scripts/SpawnerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    [SerializeField]public List<SitiosSpawneables> SpawnPoints=new List<SitiosSpawneables>();
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+[System.Serializable]
+public class SitiosSpawneables
+{
+    [SerializeField]GameObject SpawnPoint;
+    bool isEmpty;
+}
