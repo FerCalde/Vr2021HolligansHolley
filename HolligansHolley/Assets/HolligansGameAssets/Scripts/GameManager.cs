aec28019add202b0bb23219d76f92d5bb98ffdf2
Calde <chipneidev@gmail.com>
2021-05-01 18:41:28 +0200

    Datos HighScore y GameManager
    HighScore: Metodos necesarios para gestion datos
    
    GameManager: condiciones EndGame

---
+++ HolligansHolley/Assets/HolligansGameAssets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : TemporalSingleton<GameManager>
+{
+    int currentScore = 0;
+
+    int cantVidas = 3; //En caso de que queramos que los enemigos disparen y nos mate la tercera bala
+
+
+    float maxPlayTime = 60f;
+    float currentPlayTime = 0f;
+    /*  #region Singleton
+      public static GameManager Instance;
+
+      void Awake()
+      {
+          Instance = this;
+      }
+      #endregion   */
+
+
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        cantVidas = 3;
+        currentScore = 0;
+    }
+
+    /*void Update()
+    {
+        currentPlayTime += Time.deltaTime;
+    }
+    */
+
+    //Desde fixedUpdate se ejecuta en todos los procesadores a la misma velocidad, por tanto siempre nos asegura que sea igual para cada dispositivo
+    void FixedUpdate()
+    {
+        currentPlayTime += Time.fixedDeltaTime;
+        
+        //Termina tiempo Partida
+        if (currentPlayTime >= maxPlayTime)
+        {
+            EndGame();
+        }
+    }
+
+    public void UpdateCurrentScore(int points)
+    {
+        currentScore += points;
+    }
+
+    void EndGame()
+    {
+        DataScore.Instance.CheckHighScore(currentScore);
+    }
+
+}
