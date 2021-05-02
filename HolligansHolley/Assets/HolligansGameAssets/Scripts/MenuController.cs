c4d41468a12595a0781b91bd2b0e348e3c457471
Calde <chipneidev@gmail.com>
2021-05-02 02:14:21 +0200

    MenuController

---
+++ HolligansHolley/Assets/HolligansGameAssets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] GameObject panelMainMenu, panelEndGame;
+    [SerializeField] Text highScoreText;
+    
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OpenHighScore()
+    {
+        DataScore.Instance.LoadHighScore();
+        highScoreText.text = DataScore.Instance.highScore.ToString() + " points"; 
+        panelEndGame.SetActive(true);
+        panelMainMenu.SetActive(false);
+    }
+    public void OpenMenu()
+    {
+        panelMainMenu.SetActive(true);
+        panelEndGame.SetActive(false);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
