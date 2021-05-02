43384dc516e70c26ed1263fbeeb8e5968f2bb7f6
DESKTOP-7TGDIKI\uliro <uliros@live.com>
2021-05-02 22:10:59 +0200

    arte

---
+++ HolligansHolley/Assets/HolligansGameAssets/Scripts/Mirilla.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirilla : MonoBehaviour
+{
+    RaycastHit hit;
+    GameObject mainCamera;
+    [SerializeField] GameObject mira;
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    void Update()
+    {
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit))
+        {
+            mira.SetActive(true);
+            transform.position = hit.point;
+            transform.LookAt(mainCamera.transform.position);
+        }
+        else
+        {
+            mira.SetActive(false);
+        }
+    }
+}
